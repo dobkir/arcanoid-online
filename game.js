523f2c8a1cc40d3657e2e39941fbde75d44c2520
dobkir <dobkir@gmail.com>
2021-07-26 17:13:59 +0400

    Settings of coordinates for the platform and the ball

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 let game = {
   context: null,
+  width: document.querySelector("#mycanvas").getAttribute("width"),
+  height: document.querySelector("#mycanvas").getAttribute("height"),
+  platform: null,
+  ball: null,
   sprites: {
     background: null,
     ball: null,
@@ -16,8 +20,7 @@ let game = {
       this.sprites[key] = new Image();
       this.sprites[key].src = "img/" + key + ".png";
 
-      // Checking up that all images are loaded
-      // And only if all images are loaded, run a game
+      // Checking up that all images are loaded, and only then run a game
       this.sprites[key].addEventListener("load", () => {
         ++loadedSprites;
         if (loadedSprites >= requiredSprites) {
@@ -33,8 +36,9 @@ let game = {
   },
   render() {
     this.context.drawImage(this.sprites.background, 0, 0);
-    this.context.drawImage(this.sprites.ball, 0, 0);
-    this.context.drawImage(this.sprites.platform, 0, 0);
+    // The first frame of the animation.
+    this.context.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+    this.context.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
   },
   start() {
     this.init();
@@ -43,6 +47,19 @@ let game = {
     });
   }
 };
+
+game.ball = {
+  x: game.width / 2 - 20,
+  y: game.height - 85,
+  width: 40,
+  height: 40
+}
+
+
+game.platform = {
+  x: game.width / 2 - 125,
+  y: game.height - 45,
+}
 
 window.addEventListener("load", () => {
   game.start();
