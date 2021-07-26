f06e19c32d525717fa4fefe21b2a6d79b04031bf
dobkir <dobkir@gmail.com>
2021-07-26 14:30:01 +0400

    Methods: init(), preload(), run(), render(), start(). So it was a skeleton of the game created. Checking up that all images are loaded, and only then run a game.

--- game.js
+++ game.js
@@ -1,10 +1,45 @@
 let game = {
-  start: function () {
-    this.ctx = document.getElementById("mycanvas").getContext("2d");
-    let background = new Image();
-    background.src = "img/background.png";
+  context: null,
+  sprites: {
+    background: null,
+    ball: null,
+    platform: null
+  },
+  init() {
+    this.context = document.querySelector("#mycanvas").getContext("2d");
+  },
+  preload(callback) {
+    let loadedSprites = 0;
+    let requiredSprites = Object.keys(this.sprites).length;
+
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = "img/" + key + ".png";
+
+      // Checking up that all images are loaded
+      // And only if all images are loaded, run a game
+      this.sprites[key].addEventListener("load", () => {
+        ++loadedSprites;
+        if (loadedSprites >= requiredSprites) {
+          callback();
+        }
+      });
+    }
+  },
+  run() {
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(background, 0, 0);
+      this.render();
+    });
+  },
+  render() {
+    this.context.drawImage(this.sprites.background, 0, 0);
+    this.context.drawImage(this.sprites.ball, 0, 0);
+    this.context.drawImage(this.sprites.platform, 0, 0);
+  },
+  start() {
+    this.init();
+    this.preload(() => {
+      this.run();
     });
   }
 };
