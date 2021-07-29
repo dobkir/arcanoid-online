28e91ea62f0d6dce95faf83fc2a01ef739435f8c
dobkir <dobkir@gmail.com>
2021-07-29 16:25:19 +0400

    Отскок мяча от границы холста

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const KEYS = {
   SPACE: 32
 };
 
-let game = {
+const game = {
   context: null,
   width: document.querySelector("#mycanvas").getAttribute("width"),
   height: document.querySelector("#mycanvas").getAttribute("height"),
@@ -37,10 +37,10 @@ let game = {
   },
   preload(callback) {
     let loadedSprites = 0;
-    let requiredSprites = Object.keys(this.sprites).length;
+    const requiredSprites = Object.keys(this.sprites).length;
 
     // Checking up that all sprites are loaded, and only then run a game
-    let onSpriteLoad = () => {
+    const onSpriteLoad = () => {
       ++loadedSprites;
       if (loadedSprites >= requiredSprites) {
         callback();
@@ -82,6 +82,7 @@ let game = {
   updateSprites() {
     this.platform.move();
     this.ball.move();
+    this.ball.collideCanvasBounds();
     this.collideBlocks();
     this.collidePlatform();
   },
@@ -158,8 +159,8 @@ game.ball = {
   },
   collide(element) {
     // Change of coordinates on next render
-    let x = this.x + this.dx;
-    let y = this.y + this.dy;
+    const x = this.x + this.dx;
+    const y = this.y + this.dy;
 
     // Checking the collision event of the ball and the block
     if (x + this.width > element.x &&
@@ -169,6 +170,37 @@ game.ball = {
       return true;
     }
     return false;
+  },
+  // The handler of a colliding ball with canvas bounds
+  collideCanvasBounds() {
+    // Change of coordinates on next render
+    const x = this.x + this.dx;
+    const y = this.y + this.dy;
+
+    // Ball sides
+    const ballLeftSide = x;
+    const ballRightSide = ballLeftSide + this.width;
+    const ballTopSide = y;
+    const ballBottomSide = ballTopSide + this.height;
+
+    // Canvas sides
+    const canvasRightSide = game.width;
+    const canvasTopSide = 0;
+    const canvasBottomSide = game.height;
+    const canvasLeftSide = 0;
+
+    if (ballLeftSide < canvasLeftSide) {
+      this.x = 0;
+      this.dx = this.velocity;
+    } else if (ballRightSide > canvasRightSide) {
+      this.x = canvasRightSide - this.width;
+      this.dx = -this.velocity;
+    } else if (ballTopSide < canvasTopSide) {
+      this.y = 0;
+      this.dy = this.velocity;
+    } else if (ballBottomSide > canvasBottomSide) {
+      alert("Вы проиграли");
+    }
   },
   // Bumping the ball off the block
   // Here I reverse a movement by the y-axis direction of the ball. 
@@ -180,16 +212,20 @@ game.ball = {
   },
   // Bumping the ball off the platform
   bumpPlatform(platform) {
-    // Here I reverse a movement by the y-axis direction of the ball. 
-    // In this case, the angle of movement is also mirrored to the opposite angle.
-    this.dy *= -1;
-    // The further from the center is the collision of the ball, 
-    // and the platform occurs, then the sharper the angle of rebound.
 
-    // The coordinates of the point where the ball touches a platform
-    let touchX = this.x + this.width / 2;
-    // Offset on the x-axis to obtain the correct bounce angle of the ball from the platform
-    this.dx = this.velocity * platform.getTouchOffset(touchX);
+    // If the ball moving up, bumpPlatform() shouldn't act
+    if (this.dy > 0) {
+      // Here I reverse a movement by the y-axis direction of the ball. 
+      // In this case, the angle of movement is also mirrored to the opposite angle.
+      this.dy = -this.velocity;;
+      // The further from the center is the collision of the ball, 
+      // and the platform occurs, then the sharper the angle of rebound.
+
+      // The coordinates of the point where the ball touches a platform
+      let touchX = this.x + this.width / 2;
+      // Offset on the x-axis to obtain the correct bounce angle of the ball from the platform
+      this.dx = this.velocity * platform.getTouchOffset(touchX);
+    }
   }
 }
 
